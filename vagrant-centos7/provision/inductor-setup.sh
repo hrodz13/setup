dc08ea23ca42f34d5333b8cef2e9441ad0a55f27
Heriberto Rodriguez <heriberto.rodriguez13@gmail.com>
2017-02-06 20:28:53 -0600

    Configured for Azure-Refactor

--- vagrant-centos7/provision/inductor-setup.sh
+++ vagrant-centos7/provision/inductor-setup.sh
@@ -9,15 +9,8 @@ sudo rm -R "$INDUCTOR_HOME/shared"
 sudo rm "$INDUCTOR_HOME/circuit-oneops-1"
 
 sudo ln -s "$BUILD_HOME/circuit-oneops-1" "$INDUCTOR_HOME/circuit-oneops-1"
-sudo ln -s "$BUILD_HOME/circuit-main-1" "$INDUCTOR_HOME/circuit-main-1"
-sudo ln -s "$BUILD_HOME/circuit-walmartlabs-1" "$INDUCTOR_HOME/circuit-walmartlabs"
 sudo ln -s "$BUILD_HOME/oneops-admin/lib/shared" "$INDUCTOR_HOME/shared"
 
-for i in 1 2 3
-do
-    cd "$INDUCTOR_HOME/circuit-oneops-1"; circuit install
-    cd "$INDUCTOR_HOME/circuit-walmartlabs"; circuit install
-    cd "$INDUCTOR_HOME/circuit-main-1"; circuit install
-done
+cd "$INDUCTOR_HOME/circuit-oneops-1"; circuit install
 
 
